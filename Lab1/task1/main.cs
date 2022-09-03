4b870de56ab0941b7fc3bd70dea4aefbfdab3783
sk3pta <3rtugrulll@protonmail.com>
2022-09-03 23:56:16 +0300

    Initial commit

---
+++ Lab1/task1/main.cs
@@ -0,0 +1,18 @@
+﻿
+
+public class Task1
+{
+  public static void Main(string[] args)
+    {
+        Console.WriteLine($"Byte  : {byte.MinValue} -- {byte.MaxValue}\n"
+            + $"Int16 : {Int16.MinValue} -- {Int16.MinValue}\n"
+            + $"UInt16: {UInt16.MinValue} -- {UInt16.MaxValue}\n" +
+            $"char  : {(int)char.MinValue} -- {(int)char.MaxValue}\n" +
+            $"Int32 : {Int32.MinValue} -- {Int32.MaxValue}\n" +
+            $"UInt32: {UInt32.MinValue} -- {UInt32.MaxValue}\n" +
+            $"Int64 : {Int64.MinValue} --  {Int64.MaxValue}\n" +
+            $"System.Single(float) {System.Single.MinValue} -- {System.Single.MaxValue}\n" +
+            $"System.Double(float) {System.Double.MinValue} -- {System.Double.MaxValue}\n" +
+            $"System.Decimal {System.Decimal.MinValue} -- {System.Decimal.MaxValue}");
+    }
+}
---
+++ Lab1/task2/task2.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Создайте класс с именем Rectangle.
+В теле класса создайте два поля, описывающие длины сторон double side1, side2.
+Создайте пользовательский конструктор Rectangle(double sideA, double sideB), 
+в теле которого поля sideA и sideB инициализируются значениями аргументов.
+Создайте два метода, вычисляющие площадь прямоугольника - double AreaCalculator() и периметр прямоугольника - double PerimeterCalculator().
+Создайте два свойства double Area и double Perimeter с одним методом доступа get.
+Напишите программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит на экран периметр и площадь. Покройте тестами методы класса Rectangle.
+.*/
+
+public class Rectangle
+{
+    private double side1;
+    private double side2;
+    
+    public Rectangle(double sideA, double sideB)
+    {
+        this.side1 = sideA;
+        this.side2 = sideB;
+    }
+
+    public Rectangle()
+    {
+        this.side1 = 0;
+        this.side2 = 0;
+    }
+
+
+    public double AreaCalculator() { return side1 * side2; }
+
+    public double PerimeterCalculator() { return 2 * side1 + 2 * side2; }
+
+    public double Area
+    {
+        get { return this.AreaCalculator(); }
+    }
+
+    public double Perimeter
+    {
+        get { return this.PerimeterCalculator(); }
+    }
+
+}
+
+
+public class Task2
+{
+    public static void Main(string[] args)
+    {
+        Rectangle rec = new Rectangle(1,2);
+
+    }
+}
